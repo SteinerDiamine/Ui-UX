c4c71e0c5ccc5b5c4f9f873e2f1ab8ed81867bc0
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-09-12 21:04:28 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import {appleImg} from '../utils'
+import {appleImg, searchImg} from '../utils'
 
 const Navbar = () => {
   return (
     <header>
       <nav>
         <img src={appleImg} alt="Apple" width={14} height={18} />
+        <div>
+          
+         {['Phones' , 'Macbooks','Ipads', 'Apple watch'].map((nav,i) => (
+          <div key={nav}> 
+            {nav}
+          </div>
+         ))}
+        </div>
+
+        <div>
+          <img src={searchImg} alt="" />
+        </div>
+
+
       </nav>
     </header>
   )
